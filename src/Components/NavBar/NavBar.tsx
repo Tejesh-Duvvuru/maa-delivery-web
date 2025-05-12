0f28e188c3c186a888ac7ca538dbfb4bb48b22b8
Tejesh-Duvvuru <saitejesh1919@gmail.com>
2025-05-12 12:44:52 +0530

    Restructure nav component <a> to <li>

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,32 +1,50 @@
 import React from "react";
 import "./NavBar.css";
 import { FaTruck } from "react-icons/fa";
+import { Link } from "react-router";
 
 const NavBar = () => {
   return (
     <nav className="navbar-container">
       <div className="nav-logo-container">
-        <a href="#" className="logo">
+        <Link to={"/"} className="logo">
           MAA
           <br></br>DELIVERY
-        </a>
+        </Link>
       </div>
-      <div className="nav-services-container">
-        <a href="#" className="nav-anchor-element">
-          Services
-        </a>
-        <a href="#" className="nav-anchor-element">
-          Franchise
-        </a>
-        <a href="#" className="nav-anchor-element">
-          Parterns
-        </a>
-        <a href="#" className="nav-anchor-element">
-          Contact_Us
-        </a>
-        <a href="#" className="nav-anchor-element">
-          Profile
-        </a>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <ul className="nav-services-container">
+          <li>
+            <Link to={"/services"} className="nav-anchor-element">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to={"/"} className="nav-anchor-element">
+              Franchise
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="nav-anchor-element">
+              Parterns
+            </Link>
+          </li>
+          <li>
+            <Link to={"/contact"} className="nav-anchor-element">
+              Contact us
+            </Link>
+          </li>
+          <li>
+            <Link to={"/about"} className="nav-anchor-element">
+              About us
+            </Link>
+          </li>
+          <li>
+            <Link to={"/profile"} className="nav-anchor-element">
+              Profile
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
